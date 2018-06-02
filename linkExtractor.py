3ab864edd410de6448bbef485500fdd7c1a24214
ArjunRajaSekhar <36049728+ArjunRajaSekhar@users.noreply.github.com>
2018-06-02 10:29:15 +0530

    To extract links and write to file
    This extracts all the links in the website provided by user and writes it into a file for future reading and operations

--- linkExtractor.py
+++ linkExtractor.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
 from bs4.dammit import EncodingDetector
-import requests
-resp = requests.get("http://www.corporate360.us")
+import requests 
+url=input("enter the full url")
+resp = requests.get(url)
 http_encoding = resp.encoding if 'charset' in resp.headers.get('content-type', '').lower() else None
 html_encoding = EncodingDetector.find_declared_encoding(resp.content, is_html=True)
 encoding = html_encoding or http_encoding
 soup = BeautifulSoup(resp.content, from_encoding=encoding)
 
+saveToFile1=input('enter the file name')
+openFile = open(saveToFile1,'a')
 for link in soup.find_all('a', href=True):
+    openFile.write(link['href'])
+    openFile.write('\n')
     print(link['href'])
+openFile.close()
+
